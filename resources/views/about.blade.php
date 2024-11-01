21fbae1cb8f744ef0584798bba8767eebbb9c3e1
Diazalfiari <diazalfiari19@gmail.com>
2024-11-01 09:24:18 +0700

    Video 5 - Blade Component

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,23 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Halaman About</h3>
+    <p>Nama: {{ $name }}</p>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman About</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-
-    <a href="/">Home</a>
-    <a href="/blog">blog</a>
-    <a href="/about">About</a>
-    <a href="/contact">contact</a>
-    <h1>Halaman About MPJ</h1>
-    <h3>Owner: {{ $nama }}</h3>
-    <img src="img/mpj.jpg" alt="MPJ Crew" width="700">
-</body>
-
-</html>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+ 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Diaz Alfiari Rachmad']);
+    return view('about', ['name' => 'Diaz Alfiari Rachmad', 'title' => 'About']);
 });
 
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog']);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
